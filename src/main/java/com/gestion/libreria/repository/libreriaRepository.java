1c9add087e0d25e48e4d95275284209526bec52f
edison Vanegas <evanegasmunoz@gmail.com>
2026-02-28 11:35:52 -0500

    version H2

--- src/main/java/com/gestion/libreria/repository/libreriaRepository.java
+++ src/main/java/com/gestion/libreria/repository/libreriaRepository.java
@@ -3,5 +3,5 @@ package com.gestion.libreria.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gestion.libreria.entity.libro;
 
-public interface libreriaRepository extends JpaRepository<libro,Long> {
+public interface libreriaRepository extends JpaRepository<libro,Integer> {
 }
